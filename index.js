c90e1dd5738487e65c6f238d06b376dd9b132bac
Max3lord <max3.pav.loff@gmail.com>
2023-01-29 00:28:21 +0500

    add: function

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ min = (x, y) => { return x < y ? x : y};
 
 pow = (c, d) => {return c ** d};
 
+ask = (question, yes, no) => {
+	if (confirm(question)) {
+		yes();
+	} else {
+		no(); 
+	}
+}
 
+ask('Are you sure?',
+		() => {alert('Are you ready');},
+		() => {alert('Are you dont ready');}
+)
 
 // function end
