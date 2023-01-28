20895315de02ff45730564225871647e02a5cde0
Max3lord <max3.pav.loff@gmail.com>
2023-01-28 14:06:06 +0500

    fix: index

---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log('Hello World!')
+
+let array = ['one', 'two', 'three'];
+
+console.log(array.includes('one'));
+
+// function start
+
+checkAge = (age) => {(age > 18) ? console.log('Hello') : confirm('Are you sure?')};
+
+min = (a, b) => {
+	if (a > b) {
+		return b;
+	} else if (a < b) {
+		return a;
+	}
+}
+
+min = (x, y) => { return x < y ? x : y};
+
+pow = (c, d) => {return c ** d};
+
+
+
+// function end
